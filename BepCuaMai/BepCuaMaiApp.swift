aa249f3f89400b9cb1c1f42aa6fdfd7bd0af74cd
long.nguyen.van.T03 <longnv.main@gmail.com>
2023-10-05 11:04:25 +0700

    Initial Commit

---
+++ BepCuaMai/BepCuaMaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BepCuaMaiApp.swift
+//  BepCuaMai
+//
+//  Created by Long Nguyễn văn on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BepCuaMaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
